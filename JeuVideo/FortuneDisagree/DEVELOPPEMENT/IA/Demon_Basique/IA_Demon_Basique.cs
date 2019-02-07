9ec0679d1ea77209d8a4c7179768ef19ca57d42d
Faiblesse <47054556+Faiblesse@users.noreply.github.com>
2019-02-07 18:31:29 +0100

    Demon Basique

---
+++ JeuVideo/FortuneDisagree/DEVELOPPEMENT/IA/Demon_Basique/IA_Demon_Basique.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA_Demon_Basique : MonoBehaviour
+{
+	public GameObject projectile;
+	private Transform cible;
+	public Transform point_de_lancement_gauche;
+	public Transform point_de_lancement_droite;	
+	
+	private bool recharge = false;
+	public float temps_rechargement = 2.0f;
+	private float minuteur = 0.0f;
+	
+	
+	public float vitesse;
+	public float distance_reperage;
+	public float distance_tire = 50.0f;
+	
+	public bool en_mouvement = false;
+	public bool inatteignable = false;
+	private float tmps_inatteignable = 0.0f;
+	
+	private Vector2 posInitiale;
+	
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		posInitiale = transform.position;
+        cible = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if ( Mathf.Abs(cible.position.y - transform.position.y)>3.0f )
+		{
+			tmps_inatteignable += Time.deltaTime;
+			if (tmps_inatteignable> 2.0f)
+			{
+				inatteignable = true;
+			}
+		}
+		else
+		{
+			inatteignable = false;
+			tmps_inatteignable=0;
+		}
+
+		if (Vector2.Distance(transform.position, cible.position) < distance_reperage && inatteignable == false)
+		{
+			transform.position = Vector2.MoveTowards(transform.position, new Vector2(cible.position.x, transform.position.y), vitesse * Time.deltaTime);
+		}
+		else
+		{
+			transform.position = Vector2.MoveTowards(transform.position, posInitiale, vitesse * Time.deltaTime);
+		}
+		
+		if ( transform.position.x < posInitiale.x+0.5 && transform.position.x > posInitiale.x-0.5)
+		{
+			en_mouvement = false;
+		}
+		else
+		{
+			en_mouvement = true;
+		}
+
+		if ( Vector2.Distance(transform.position, cible.position) < distance_tire && en_mouvement == false)
+		{
+			if (recharge == true)
+			{
+				minuteur += Time.deltaTime;
+				if (minuteur > temps_rechargement)
+				{
+					recharge = false;
+				}
+			}
+			else
+			{
+				GameObject tire;
+				recharge = true;
+				minuteur = 0;
+				if (transform.position.x-cible.position.x >0)
+				{
+					tire = Instantiate(projectile, point_de_lancement_gauche.position, Quaternion.identity);
+				}
+				else
+				{
+					tire = Instantiate(projectile, point_de_lancement_droite.position, Quaternion.identity);
+				}
+			}
+		}
+		
+    }
+}
---
+++ JeuVideo/FortuneDisagree/DEVELOPPEMENT/IA/Demon_Basique/IA_Tire_Basique.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA_Tire_Basique : MonoBehaviour
+{
+	public float vitesse;
+	
+	public float temps_de_vie = 7.0f;
+	private float minuteur = 0.0f;
+	
+	private Transform cible;
+	private Vector2 but;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        cible = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+		but = cible.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        minuteur += Time.deltaTime;
+		if (minuteur>temps_de_vie)
+		{
+			Destroy(gameObject);
+		}
+		
+		transform.position = Vector2.MoveTowards(transform.position, but, vitesse * Time.deltaTime);
+    }
+	
+}
