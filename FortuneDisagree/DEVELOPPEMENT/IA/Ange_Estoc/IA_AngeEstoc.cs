d90a1a556d29464948d9ed9c9ea4a56924391c49
Rastignac <idealismenoir@gmail.com>
2019-02-24 14:22:53 +0100

    Debug IA Demon/Ange

--- FortuneDisagree/DEVELOPPEMENT/IA/Ange_Estoc/IA_AngeEstoc.cs
+++ FortuneDisagree/DEVELOPPEMENT/IA/Ange_Estoc/IA_AngeEstoc.cs
@@ -4,40 +4,33 @@ using UnityEngine;
 
 public class IA_AngeEstoc : MonoBehaviour
 {
+	public GameObject zoneNormale;
+	public GameObject zoneCharge;
+	
 	private Transform cible;	
 	
-	public bool peut_charge = true;
-	public bool prepare_charge = false;
-	public bool charge = false;
-	public bool a_charge = false;
+	private float minuteur = 0.0f;
+	public float chargement = 5.0f;
+	public bool peut_charge = false;
 	
-	public float temps_prepare_charge = 1.5f;
-	private float minuteur_prepare_charge = 0.0f;
+	private float minuteur_va_charge = 0.0f;
+	public float chargement_va_charge = 1.0f;
+	public bool va_charge = false;
 	
-	public float temps_charge = 1.0f;
 	private float minuteur_charge = 0.0f;
-	
-	public float temps_rechargement = 5.0f;
-	private float minuteur_rechargement = 0.0f;
-	
-	public float hauteur_detection_vertical = 5.0f;
-	
-	
-	public float vitesse;
-	public float vitesse_precharge;
-	public float vitesse_charge;
-	
-	public float distance_reperage;
-	public float distance_mini_charge;
-	
-	private Vector2 posInitiale;
-	private float posCharge;
+	public float chargement_charge = 3.0f;
+	public bool charge = false;
 	
 	private float V;
+	public float vitesse = 5.0f;
+	public float vitesse_va_charge = 1.0f;
+	public float vitesse_charge = 25.0f;
+	
+	public Vector2 posInitiale;
+	public Vector2 posCharge;
 	
     void Start()
     {
-		V = vitesse;
 		posInitiale = transform.position;
         cible = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
@@ -45,78 +38,66 @@ public class IA_AngeEstoc : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		V = vitesse;
-		
-		if (peut_charge && transform.position.y - cible.position.y < hauteur_detection_vertical && ( (transform.position.x-cible.position.x >= distance_mini_charge  && transform.position.x-cible.position.x <= distance_reperage ) || (transform.position.x-cible.position.x <= -distance_mini_charge  && transform.position.x-cible.position.x >= -distance_reperage )))
+		if ( peut_charge == false )
 		{
-			prepare_charge = true;
-		}
-		else
-		{
-			if (transform.position.y - cible.position.y < hauteur_detection_vertical && (transform.position.x-cible.position.x <= distance_reperage || transform.position.x-cible.position.x >= -distance_reperage))
+			minuteur += Time.deltaTime;
+			
+			if (minuteur > chargement)
 			{
-				transform.position = Vector2.MoveTowards(transform.position, new Vector2(cible.position.x, transform.position.y), vitesse_precharge * Time.deltaTime);
-			}
-			else
-			{
-				transform.position = Vector2.MoveTowards(transform.position, posInitiale, V * Time.deltaTime);
+				minuteur = 0.0f;
+				peut_charge = true;
 			}
 		}
 		
-		if (prepare_charge)
+		if (va_charge == false)
 		{
-			peut_charge = false;
-			
-			minuteur_prepare_charge += Time.deltaTime;
-			V = vitesse_precharge;
-			
-			if (minuteur_prepare_charge >= temps_prepare_charge)
+			if ( zoneNormale.GetComponent<Zone_Estoc>().active == false && zoneCharge.GetComponent<Zone_Estoc>().active == false)
 			{
-				prepare_charge = false;
-				charge = true;
-				
-				minuteur_prepare_charge = 0;
-				
-				posCharge = cible.position.x;
+				transform.position = Vector2.MoveTowards(transform.position, new Vector2 (posInitiale.x, transform.position.y), vitesse * Time.deltaTime);
+			}
+			
+			if ( zoneNormale.GetComponent<Zone_Estoc>().active && zoneCharge.GetComponent<Zone_Estoc>().active)
+			{
+				transform.position = Vector2.MoveTowards(transform.position, new Vector2 (cible.position.x, transform.position.y), vitesse * Time.deltaTime);
+			}
+			
+			if ( zoneNormale.GetComponent<Zone_Estoc>().active == false && zoneCharge.GetComponent<Zone_Estoc>().active)
+			{	
+				va_charge = true;
+			}
+		}
+		
+		if (va_charge)
+		{
+			if ( zoneNormale.GetComponent<Zone_Estoc>().active )
+			{
+				va_charge = false;
+				minuteur_va_charge = 0.0f;
 			}
 			else
 			{
-				transform.position = Vector2.MoveTowards(transform.position, new Vector2(cible.position.x, transform.position.y), V * Time.deltaTime);
+				transform.position = Vector2.MoveTowards(transform.position, new Vector2 (cible.position.x, transform.position.y), vitesse_va_charge * Time.deltaTime);
+				minuteur_va_charge += Time.deltaTime;
+				if (minuteur_va_charge > chargement_va_charge)
+				{
+					va_charge = false;
+					charge = true;
+					minuteur_va_charge = 0.0f;
+					posCharge = cible.position;
+				}
 			}
 		}
 		
 		if (charge)
 		{
-			minuteur_charge += Time.deltaTime;
-			V = vitesse_charge;
-			
-			if (minuteur_charge >= temps_charge)
+			transform.position = Vector2.MoveTowards(transform.position, new Vector2 (posCharge.x, transform.position.y), vitesse_charge * Time.deltaTime);
+			minuteur_charge+=Time.deltaTime;
+			if (minuteur_charge > chargement_charge)
 			{
-				charge = false;
-				a_charge = true;
-				
 				minuteur_charge = 0;
-			}
-			else
-			{
-				transform.position = Vector2.MoveTowards(transform.position, new Vector2(posCharge, transform.position.y), V * Time.deltaTime);
+				charge = false;
+				peut_charge = false;
 			}
 		}
-		
-		if (a_charge)
-		{
-			minuteur_rechargement += Time.deltaTime;
-			V = vitesse;
-			
-			if (minuteur_rechargement >= temps_charge)
-			{
-				a_charge = false;
-				peut_charge = true;
-				
-				minuteur_rechargement = 0;
-			}
-		}
-		
-		
     }
 }
